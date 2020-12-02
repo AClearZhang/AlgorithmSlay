8b7eb3f1556238f92c28e44aa443bb667b91c126
aclearzhang <282971753@qq.com>
2020-12-02 22:26:50 +0800

    substr ready

---
+++ C++/438.Find All Anagrams in a String.cpp
@@ -0,0 +1,69 @@
+/*
+ * @FilePath            : \Algorithm&Interview\AlgorithmSlay\C++\438.Find All Anagrams in a String.cpp
+ * @Author              : AClearZhang
+ * @Date                : 2020-12-02 22:24:19
+ * @E-mail              : aclearzhang@qq.com
+ * @Homepage            : www.aclear.top
+ * @LastEditors         : AClearZhang
+ * @LastEditTime        : 2020-12-02 22:25:57
+ * @Version             : 1.0
+ * @Description         : 找出异位词
+ * 给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
+
+字符串只包含小写英文字母，并且字符串 s 和 p 的长度都不超过 20100。
+
+说明：
+
+字母异位词指字母相同，但排列不同的字符串。
+不考虑答案输出的顺序。
+示例 1:
+
+输入:
+s: "cbaebabacd" p: "abc"
+
+输出:
+[0, 6]
+
+解释:
+起始索引等于 0 的子串是 "cba", 它是 "abc" 的字母异位词。
+起始索引等于 6 的子串是 "bac", 它是 "abc" 的字母异位词。
+ 示例 2:
+
+输入:
+s: "abab" p: "ab"
+
+输出:
+[0, 1, 2]
+
+解释:
+起始索引等于 0 的子串是 "ab", 它是 "ab" 的字母异位词。
+起始索引等于 1 的子串是 "ba", 它是 "ab" 的字母异位词。
+起始索引等于 2 的子串是 "ab", 它是 "ab" 的字母异位词。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-all-anagrams-in-a-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+
+    }
+};
+
+int main()
+{
+    
+    system("pause");
+    return 0;
+}
+
