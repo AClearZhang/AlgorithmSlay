6da05f854ff1c282857d3a270f49b45eb5ac0ab7
aclearzhang <282971753@qq.com>
2020-12-02 17:00:52 +0800

    76 comple v2

--- C++/76.MinimumWindowSubstring.cpp
+++ C++/76.MinimumWindowSubstring.cpp
@@ -1,11 +1,11 @@
 /*
- * @FilePath            : \project\AlgorithmSlay\C++\76.MinimumWindowSubstring.cpp
+ * @FilePath            : \Algorithm&Interview\AlgorithmSlay\C++\76.MinimumWindowSubstring.cpp
  * @Author              : AClearZhang
  * @Date                : 2020-11-28 00:06:16
  * @E-mail              : aclearzhang@qq.com
  * @Homepage            : www.aclear.top
  * @LastEditors         : AClearZhang
- * @LastEditTime        : 2020-12-02 00:51:40
+ * @LastEditTime        : 2020-12-02 16:55:15
  * @Version             : 1.0
  * @Description         : 最小覆盖子串 —— 滑动窗口方法
  * 给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
@@ -88,7 +88,7 @@ using namespace std;
 // };
 
 /**
- * @description: 所有字符的最小子串，不存在重复字符。
+ * @description: 所有字符的最小子串，~不存在重复字符。~   输入字符串是可以重复的！
  * @param {*} 
  * @return {*}
  * @notes: 
@@ -100,50 +100,75 @@ public:
         // while( *pre!= s[s.size()-1]){
             
         // }
-        unordered_map<char, int> need, window; // need.count('A')>0 且 直接++ 就是0++；// 注意window 只包括有效子串
+        unordered_map<char, int> need, window; // need最小子串有效字符，window也是有效最小子串字符；左右窗口大小由 [left, right)控制
         int left=0, right=0, valid=0;
         for(char c:t) need[c]++;
+        // cout << "need size is : "  << need.size() << endl;
+
         int start = 0, len = INT_MAX;
 
         while( right < s.size() ){
             // 右边的添加到窗口  扩大窗口
-            window[s[right]]++;
+            char r = s[right];
+            // window[s[right]]++;
             right++;                // 所以数据集是左闭右开的
 
             // 进行窗口内数据的一系列更新
             // if(window.count(s[right-1]) <= need.count(s[right-1]) && need.count(s[right-1])>0){
             //     valid++;
             // }
-            if(window.count(s[right-1])==1 && need.count(s[right-1])>0){
-                // 如果是新的一个字符类别 则valid++
-                valid++;
+            // 对左边窗口进行变化
+            if(need.count(r) > 0){   // 会自动添加进来，设置为0了！ NO！删除 //need[r] > 0
+                window[r]++;
+                if( window[r] == need[r])
+                    valid++;
             }
 
             /*** debug 输出的位置 ***/
-            printf("window: [%d, %d)\n", left, right);
+            // printf("window: [%d, %d)\n", left, right);
             /********************/
-        
+            // for(pair<char,int> needC : need){
+            //     cout<< "first:" << needC.first << "second:" << needC.second << " " ;
+            // }
+            // cout << endl;
+            
+            // cout << "need size0 is : "  << need.size() << endl;
+            // cout << "valid is : "  << valid << endl;
+            
             //左边开始减少窗口
-            while( valid == need.size() ){   
+            // if( window[r] == need[r])
+            //         valid++;  
+            // 有以上判断！   所以直接看 need.size() 即可
+
+            while( valid == need.size() ){ 
+                // for(pair<char,int> needC : need){
+                //     cout<< "first:" << needC.first << "second:" << needC.second << " " ;
+                // }
+                // cout << endl;
+                // cout << "t size is : "  << t.size() << endl;
+                // cout << "need size is : "  << need.size() << endl;
+                // cout << "window size is : "  << window.size() << endl;
+
                 if( right-left < len ){
                     len = right - left;
                     start = left;
                 }
-                window[s[left]]--;
+                // window[s[left]]--;
+                char l = s[left];
                 left++;
-                // 进行窗口内数据的一系列更新
-                // 1. 首先valid需要更新
-                // 2. 然后更新 len
-                if(window.count(s[left-1])==0 && need.count(s[left-1])>0){
-                // 如果是新的一个字符类别 则valid++
-                    valid--;
+                
+                // 更新窗口 以及 最终结果判定
+                if(need.count(l) > 0){
+                    window[l]--;
+                    if(window[l] < need[l])
+                        valid--;
                 }
                 
             }
-
-
         }
-        return len == INT_MAX ?"":s.substr(start, len); 
+        
+        return len==INT_MAX? "" : s.substr(start, len) ;
+        // return len == INT_MAX ?"":s.substr(start, len); 
 
     }
 };
@@ -176,7 +201,7 @@ public:
             }
 
             /*** debug 输出的位置 ***/
-            // printf("window: [%d, %d)\n", left, right);
+            printf("window++: [%d, %d)\n", left, right);
             /********************/
         
             //左边开始减少窗口
@@ -200,7 +225,7 @@ public:
                         valid--;
                     }
                 }
-                // printf("window1: [%d, %d)\n", left, right);
+                printf("window--: [%d, %d)\n", left, right);
                 
             }
 
@@ -213,9 +238,10 @@ public:
 
 
 int main(){
-    string s="ADOBECODEBANC", t="ABC";
+    // string s="ADOBECODEBANC", t="ABC";
+    string s="aa", t="aa";
     // cout << s.size()<< "  " << t.size() << endl;
-    Solution so;
+    Solution1 so;
     string rel = so.minWindow(s,t);
     cout << "最小子串为：" << rel << endl;
 
