1264bb224c6f163c9f12230323ece2ac3a33fb99
aclearzhang <282971753@qq.com>
2020-12-16 10:33:25 +0800

    dfs1 comv1

--- C++/215.KthLargestEleInArray.cpp
+++ C++/215.KthLargestEleInArray.cpp
@@ -19,60 +19,68 @@
 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
 输出: 4
  */
+#include <algorithm>
 #include <iostream>
 #include <map>
+#include <unordered_map>
 #include <vector>
 #include <windows.h>
-#include <unordered_map>
-#include <algorithm>
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool static comp(int a, int b){
-        return a>b; 
+    bool static comp(int a, int b)
+    {
+        return a > b;
     }
     /**
      * @Description: 方法二——注意在类内 由于需要一个全局的静态成员函数， 所以加了static；进行从大到小的比较。
      * @Param: 
      * @Return: 
-     * @Notes: 
+     * @Notes: 24ms  the BEST！
      */
-    int findKthLargest1(vector<int>& nums, int k) {
+    int findKthLargest1(vector<int> &nums, int k)
+    {
         //从大到小排序 —— 找到 k-1 个未知的数。
         int n = nums.size();
-        sort(nums.begin(), nums.begin()+n, comp);
-        return nums[k-1];
+        sort(nums.begin(), nums.begin() + n, comp);
+        return nums[k - 1];
     }
-    
+
     /**
      * @Description: 下面通过自定义快排序实现 从大到小排序。
      * @Param: 
      * @Return: 
-     * @Notes: 
+     * @Notes: 180ms
      */
-    int findKthLargest(vector<int>& nums, int k) {
+    int findKthLargest(vector<int> &nums, int k)
+    {
         //从大到小排序 —— 找到 k-1 个未知的数。
-        QuickSort(nums, 0, nums.size()-1);
-        return nums[k-1];
-
+        QuickSort(nums, 0, nums.size() - 1);
+        return nums[k - 1];
     }
-    void QuickSort(vector<int> &nums, int left, int right){
-        if(left < right){//左闭右闭   从大到小排序
+    void QuickSort(vector<int> &nums, int left, int right)
+    {
+        if (left < right)
+        { //左闭右闭   从大到小排序
             //相当于先序遍历， 先排好一个位置，然后继续搜索左右分别开工排序。一个个位置
             int pivot = Partition(nums, left, right);
-            QuickSort(nums,left, pivot-1);
-            QuickSort(nums,pivot+1, right);
+            QuickSort(nums, left, pivot - 1);
+            QuickSort(nums, pivot + 1, right);
         }
-
     }
-    int Partition(vector<int> &nums,int left, int right){
+    int Partition(vector<int> &nums, int left, int right)
+    {
         int pivot = nums[left];
-        while(left < right){
-            while( left<right && nums[right]<=pivot ) right--;
+        while (left < right)
+        {
+            while (left < right && nums[right] <= pivot)
+                right--;
             nums[left] = nums[right];
-            while( left<right && nums[left]>=pivot ) left++;
+            while (left < right && nums[left] >= pivot)
+                left++;
             nums[right] = nums[left];
         }
         nums[left] = pivot;
@@ -84,55 +92,84 @@ public:
      * @Return: 
      * @Notes: 注意不同于快速排序的地方——快速选择只需要找到第k大的枢轴值(pivot)即可，然后  再在大于或小于的方向缩小范围寻找 “位置”。
      */
-    int findKthLargest2(vector<int>& nums, int k) {
+    int findKthLargest2(vector<int> &nums, int k)
+    {
         // 仍然从小到大快速选择；找到导数第k个， 即正数第nums.size()-k+1个；但是需要数组从零开始所以是数组 nums.size()-k
-        int l=0,r = nums.size() -1, target = nums.size()-k;
-        while(l<r){
+        int l = 0, r = nums.size() - 1, target = nums.size() - k;
+        while (l < r)
+        {
             int pivot = quickSelection(nums, l, r);
-            if(pivot == target)  return nums[pivot];
-            else if(pivot > target) r = pivot - 1;
-            else{
+            if (pivot == target)
+                return nums[pivot];
+            else if (pivot > target)
+                r = pivot - 1;
+            else
+            {
                 l = pivot + 1;
             }
         }
         // 如果说没找到l==r了；返回 此时位置即可。
         return nums[l];
     }
-    // 辅助函数 —— 快速选择位置
-    int quickSelection(vector<int> &nums, int l ,int r){
+    // 辅助函数 —— 快速选择位置 
+    // 52ms
+    int quickSelection(vector<int> &nums, int l, int r)
+    {
         int pivot = nums[l];
-        while(l<r){
-            while(l<r && nums[r] >= pivot) r--;
+        while (l < r)
+        {
+            while (l < r && nums[r] >= pivot)
+                r--;
             nums[l] = nums[r];
-            while(l<r && nums[l] <= pivot) l++;
+            while (l < r && nums[l] <= pivot)
+                l++;
             nums[r] = nums[l];
         }
         // 当前l为枢轴值，定了
-        nums[l]=pivot;
+        nums[l] = pivot;
         return l;
     }
-
-
-
-    
+    // 辅助函数 —— 快速选择c++ 大佬做题
+    //不好136ms
+    int quickSelection101(vector<int> &nums, int l, int r)
+    {
+        int i = l + 1, j = r;
+        while (true)
+        {
+            while (i < r && nums[i] <= nums[l])
+            {
+                ++i;
+            }
+            while (l < j && nums[j] >= nums[l])
+            {
+                --j;
+            }
+            if (i >= j)
+            {
+                break;
+            }
+            swap(nums[i], nums[j]);
+        }
+        swap(nums[l], nums[j]);
+        return j;
+    }
 };
 
-int main(){
+int main()
+{
     // vector<int> arr = {3,2,1,5,6,4};
-    vector<int> arr = {3,1,2,4};
+    vector<int> arr = {3, 1, 2, 4};
     // int k = 2;
     int k = 2;
     Solution so;
     // int rel = so.findKthLargest1(arr, k);
     int rel = so.findKthLargest2(arr, k);
-    cout<<"result is :" << rel << endl;
-    for(auto a:arr){
+    cout << "result is :" << rel << endl;
+    for (auto a : arr)
+    {
         cout << a << endl;
     }
 
     system("pause");
     return 0;
 }
-
-
-
--- C++/695.MaxAreaOfIsland.cpp
+++ C++/695.MaxAreaOfIsland.cpp
@@ -5,7 +5,7 @@
  * @E-mail              : aclearzhang@qq.com
  * @Homepage            : www.aclear.top
  * @LastEditors         : AClearZhang
- * @LastEditTime        : 2020-12-15 22:34:57
+ * @LastEditTime        : 2020-12-16 10:22:33
  * @Version             : 1.0
  * @Description         : 岛屿的最大面积
  * 695. 岛屿的最大面积
@@ -38,33 +38,77 @@
 
 注意: 给定的矩阵grid 的长度和宽度都不超过 50。
  */
+#include <algorithm>
 #include <iostream>
 #include <map>
+#include <unordered_map>
 #include <vector>
 #include <windows.h>
-#include <unordered_map>
-#include <algorithm>
 
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
+    /**
+     * @Description: 使用深度优先搜索递归调用； 相当于先序遍历 —— 先搜索当前的位置，然后再深入/递归到 循环调用自身，找到周围的岛屿 ++。
+     * @Param: 
+     * @Return: 
+     * @Notes: 
+     */
+    int maxAreaOfIsland(vector<vector<int>> &grid)
+    {
+        int max = 0, now = 0;
+        for (int i = 0; i < grid.size(); i++)
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                //r c 找到位置下面开始dfs
+                if (grid[i][j])
+                {
+                    now = dfs(grid, i, j);
+                    max = max < now ? now : max;
+                }
+            }
+        return max;
+    }
+protected:
+    int dfs(vector<vector<int>> &grid, int r, int c)
+    {
+        int rel = 0;
 
+        // 限定防止在边缘  或者 跑出递归的边界
+        // 注意边缘也是可以为1的！
+        if (r > -1 && r < grid.size() && c > -1 && c < grid[0].size() && grid[r][c])
+        {
+            int di[] = {-1, 1, 0, 0}, dj[] = {0, 0, -1, 1}; //分别对应上下左右深搜方向
+            // 相当于先序遍历开始；
+            grid[r][c] = 0;
+            rel += 1;
+            // 再进行深搜
+            for (int i = 0; i < 4; i++)
+            {
+                rel += dfs(grid, r + di[i], c + dj[i]);
+            }
+        }
+        return rel;
     }
 };
 
-
-int main(){
-    // vector<int> arr = {1,1,1,2,2,3};
-    vector<int> arr = {1};
-    // int k = 2;
-    int k = 1;
+int main()
+{
+    vector<vector<int>> arr = { {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0} };
+    vector<vector<int>> arr1 = {{0,0,0,0,0,0,0,0}};
+    vector<vector<int>> arr2 = {{1,1,0,0,0},{1,1,0,0,0},{0,0,0,1,1},{0,0,0,1,1}};
     Solution so;
-    auto rel = so.topKFrequent(arr, k);
-    for(auto r:rel){
-        cout << r << endl;
-    }
-
+    // int maxArea = so.maxAreaOfIsland(arr);
+    int maxArea = so.maxAreaOfIsland(arr2);
+    cout << "当前最大的面积为：" << maxArea << endl;    
     system("pause");
     return 0;
 }
