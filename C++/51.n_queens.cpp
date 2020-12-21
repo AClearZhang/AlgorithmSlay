947f78fc81c855a2d190955fb92047cb1c5353a0
aclearzhang <282971753@qq.com>
2020-12-21 23:18:25 +0800

    add 51 n queens

---
+++ C++/51.n_queens.cpp
@@ -0,0 +1,11 @@
+/*
+ * @FilePath            : \Algorithm&Interview\AlgorithmSlay\C++\51.n_queens.cpp
+ * @Author              : AClearZhang
+ * @Date                : 2020-12-21 22:51:42
+ * @E-mail              : aclearzhang@qq.com
+ * @Homepage            : www.aclear.top
+ * @LastEditors         : AClearZhang
+ * @LastEditTime        : 2020-12-21 22:51:44
+ * @Version             : 1.0
+ * @Description         : 
+ */
