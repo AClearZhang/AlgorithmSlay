424f86796f4ff085de5400024556ff2c2d18fa3e
aclearzhang <282971753@qq.com>
2020-12-21 16:14:47 +0800

    79 wordsearch complev1

--- C++/77.combinations.cpp
+++ C++/77.combinations.cpp
@@ -7,5 +7,119 @@
  * @LastEditors         : AClearZhang
  * @LastEditTime        : 2020-12-20 22:11:51
  * @Version             : 1.0
- * @Description         : 
+ * @Description         : 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+
+示例:
+
+输入: n = 4, k = 2
+输出:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/combinations
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
+
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @Description: 46是为排列，本题为组合
+     * @Param: 
+     * @Return: 
+     * @Notes: 
+     */
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> comb(k,0); // 记录
+        int count = 0;
+        backtracking(ans, comb, count, 1, n, k);
+        // recallTree(n,k,0, track, ans);
+        return ans;
+    
+    }
+    /**
+     * @Description: 大佬的做法
+     * @Param: 
+     * @Return: 
+     * @Notes: 
+     */
+    void backtracking(vector<vector<int>> &ans, vector<int> &comb, int &count, int pos, int n, int k){
+        if(count == k){
+            ans.push_back(comb);
+            return ;
+        }
+        // pos是下一个位置到末尾，所以不会 重复。yes 我也想到了
+        // 只不过我的有剪枝
+        for( int i =  pos; i <= n ; ++i){
+            comb[count++] = i; //修改当前节点状态
+            backtracking(ans, comb, count, i+1, n, k);
+            --count;
+        }
+    }
+
+    /**
+     * @Description: 我的做法—— l1——n； 到  n-k+1+(l1-1)
+     * @Param: 
+     * @Return: 
+     * @Notes: 
+     */    
+    void recallTreeMe(int n, int k, int level, vector<int> &track, vector<vector<int>> &ans)
+    {
+        if (track.size() == nums.size())
+        {
+            ans.push_back(track);
+            return;
+        }
+
+        // 选择列表的选择。
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (find(track.begin(), track.end(), nums[i]) != track.end())
+            {
+                // 找到了
+                continue;
+            }
+            // 未找到进行添加，路径 添加
+            track.push_back(nums[i]);
+            recallTree(nums, track, ans);
+            // 还原track
+            track.pop_back();
+        }
+    }
+};
+
+int main()
+{
+
+    vector<int> arr = {1, 2, 3};
+    Solution so;
+    auto rel = so.permute(arr);
+    printf("输出结果为：/n");
+    for (auto r : rel)
+    {
+        cout << "[ ";
+        for (auto r2 : r)
+        {
+            cout << r2 << ", ";
+        }
+        cout << " ]" << endl;
+    }
+
+    system("pause");
+    return 0;
+}
---
+++ C++/79.WordSearch.cpp
@@ -0,0 +1,126 @@
+/*
+ * @FilePath            : \Algorithm&Interview\AlgorithmSlay\C++\79.WordSearch.cpp
+ * @Author              : AClearZhang
+ * @Date                : 2020-12-21 10:13:34
+ * @E-mail              : aclearzhang@qq.com
+ * @Homepage            : www.aclear.top
+ * @LastEditors         : AClearZhang
+ * @LastEditTime        : 2020-12-21 16:13:02
+ * @Version             : 1.0
+ * @Description         : 79. 单词搜索
+给定一个二维网格和一个单词，找出该单词是否存在于网格中。
+
+单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+
+ 
+
+示例:
+
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+
+给定 word = "ABCCED", 返回 true
+给定 word = "SEE", 返回 true
+给定 word = "ABCB", 返回 false
+ * 
+ */
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> direction{-1, 0, 1, 0, -1};
+    /**
+     * @Description: 搜索是否粗拿在子字符串
+     * @Param: 
+     * @Return: 
+     * @Notes: 
+     */
+    bool exist(vector<vector<char>> &board, string word)
+    {
+        // 注意 原始边界！
+        if (board.empty())
+            return false;
+
+        // vector<char> track;
+        int count = 0;              // 此处不用 计数的，只需要看位置。
+        bool rel = false;
+        int m = board.size(), n = board[0].size();
+        vector<vector<bool>>  visited(m, vector<bool>(n, false));
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                dfsWordExit(board, i, j, rel, count, visited, word);
+            }
+        }
+        return rel;
+    }
+    /**
+     * @Description: 对应辅助函数，进行深搜 并寻找单词。
+     * @Param: 
+     * @Return: 通过 rel 是否存在返回：true or false;
+     * @Notes: 
+     */
+    void dfsWordExit(vector<vector<char>> &board, int r, int c, bool &rel, int count, vector<vector<bool>> &visited,string &word)
+    {
+        // 关键在深搜
+        // 边缘结束
+        if(r<0 || r>=board.size() || c<0 || c>=board[0].size()||count>word.length() || rel || visited[r][c])  return;
+        if(board[r][c]!=word[count]) return ;
+        // 结束条件
+        if (word.length()-1 == count)  // 因为有了上面那个条件，所以这可以用-1；
+        {
+            rel = true;
+            return;
+        }
+
+        //do
+        if (board[r][c] == word[count])   //边缘没处理好 使得出现语法错误！不应该
+        {
+            cout << "i am in and word is:" << word[count] << endl; 
+            // 才开始深搜
+            count++;
+            visited[r][c] = true;
+            for (int k = 0; k < 4; ++k)
+            {
+                dfsWordExit(board, r + direction[k], c + direction[k + 1], rel, count, visited, word);
+            }
+            // 还原错误了
+            // 回溯返回状态
+            count--;
+            visited[r][c] = false;
+        }
+
+        return;
+    }
+};
+
+int main()
+{
+    vector<vector<char>> arr = {{'A', 'B', 'C', 'E'},
+        {'S', 'F', 'C', 'S'},
+        {'A', 'D', 'E', 'E'}};
+    // string word = "ABCCED";
+    string word = "ABCB";   //会报错，注意已经访问过的  不要再重复访问了。
+    // for(int i = 0;i<word.length();++i){
+    //     cout<<word[i] << endl;
+    // }
+    Solution so;
+    auto rel = so.exist(arr, word);
+    cout << "是否存在当前word：" << rel << endl;
+    system("pause");
+    return 0;
+}
