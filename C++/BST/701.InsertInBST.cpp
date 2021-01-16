96541b768c5392c0e47e148697ea408f3746039b
aclearzhang <282971753@qq.com>
2021-01-16 11:32:23 +0800

    insert bst v1

--- C++/BST/701.InsertInBST.cpp
+++ C++/BST/701.InsertInBST.cpp
@@ -5,9 +5,10 @@
  * @E-mail              : aclearzhang@qq.com
  * @Homepage            : www.aclear.top
  * @LastEditors         : AClearZhang
- * @LastEditTime        : 2021-01-14 17:17:01
+ * @LastEditTime        : 2021-01-16 11:32:10
  * @Version             : 1.0
- * @Description         : 
+ * @Description         : 二叉树中插入操作
+ * 
  */
 
 
@@ -30,3 +31,28 @@ struct TreeNode {
 };
 
 
+class Solution {
+public:
+    /**
+     * @Description: 简单点 插入在叶子节点
+     * @param {*}
+     * @return {*}
+     * @notes: 
+     */
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == nullptr)  return new TreeNode(val);
+        
+        // 如果存在 则返回避免重复插入
+        if(val == root->val) return root;
+        else if(val < root->val) root->left = insertIntoBST(root->left, val);
+        else if(val > root->val) root->right = insertIntoBST(root->right, val);
+        return root;
+    }
+};
+
+
+int main(){
+    
+    system("pause");
+    return 0;
+}
