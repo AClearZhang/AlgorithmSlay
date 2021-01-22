98dc194c55bb8b4fce1e63b52a1df5a4dd80e5eb
aclearzhang <282971753@qq.com>
2021-01-22 15:27:51 +0800

    回文v1&v2

--- C++/LinkedList/234.PalindromeList.cpp
+++ C++/LinkedList/234.PalindromeList.cpp
@@ -5,7 +5,7 @@
  * @E-mail              : aclearzhang@qq.com
  * @Homepage            : www.aclear.top
  * @LastEditors         : AClearZhang
- * @LastEditTime        : 2021-01-22 00:50:35
+ * @LastEditTime        : 2021-01-22 15:27:33
  * @Version             : 1.0
  * @Description         : 判断链表是否回文
  * 234. 回文链表
@@ -49,7 +49,7 @@ public:
      * @return {*}
      * @notes: 
      */
-    bool isPalindrome(ListNode* head) {
+    bool isPalindrome1(ListNode* head) {
         if(head == nullptr) return true;
         // 寻找中点
         int mid = 0;
@@ -79,4 +79,43 @@ public:
         if(mid->val != tmp->val) flag = false;  //【注意】这里2
         return tmp->next;    //【注意】这里3
     }
+    /**
+     * @Description: 101 dalao的方法：先用快慢指针赵道链表中点，然后链表切成两半；
+     *              再把后半段翻转；最后 比较两段是否相等。
+     * @param {*}
+     * @return {*}
+     * @notes: 
+     */
+    bool isPalindrome(ListNode* head) {
+        if(!head||!head->next){
+            return true;
+        }
+        // 快2倍 找链表中点
+        ListNode* slow = head,*fast = head;
+        while(fast->next && fast->next->next){
+            // 必须要求下下个都要有，因为走两步
+            slow = slow->next;  // 此时下一个即为中点 要反转的链表开头。
+            fast = fast->next->next;
+        }
+        slow->next = reverseList(slow->next, nullptr);
+        slow = slow->next;
+        // 比较
+        while(slow){
+            if(slow->val != fast->val) return false;
+            slow = slow->next;
+            fast = fast->next;
+        }
+        // 全部相等
+        return true;
+
+    }
+    // 辅助函数 —— 翻转链表
+    ListNode* reverseList(ListNode* head, ListNode* prev){
+        if( head == nullptr) return prev;
+        // 开始递归两两前序翻转 （而这里没有回溯
+        ListNode* next = head->next;
+        head->next = prev;
+        return reverseList(next, head);
+    }
+    
 };
