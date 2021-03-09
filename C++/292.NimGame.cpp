d9a04e1587815936826be8c060f2fdf4740c04b9
aclearzhang <282971753@qq.com>
2021-03-09 17:49:32 +0800

    one line commit games 3 problems

---
+++ C++/292.NimGame.cpp
@@ -0,0 +1,52 @@
+/*
+ * @FilePath            : \Algorithm&Interview\AlgorithmSlay\C++\292.NimGame.cpp
+ * @Author              : AClearZhang
+ * @Date                : 2021-03-09 17:19:23
+ * @E-mail              : aclearzhang@qq.com
+ * @Homepage            : www.aclear.top
+ * @LastEditors         : AClearZhang
+ * @LastEditTime        : 2021-03-09 17:19:46
+ * @Version             : 1.0
+ * @Description         : 智力游戏1
+ * 292. Nim 游戏
+你和你的朋友，两个人一起玩 Nim 游戏：
+
+桌子上有一堆石头。
+你们轮流进行自己的回合，你作为先手。
+每一回合，轮到的人拿掉 1 - 3 块石头。
+拿掉最后一块石头的人就是获胜者。
+假设你们每一步都是最优解。请编写一个函数，来判断你是否可以在给定石头数量为 n 的情况下赢得游戏。如果可以赢，返回 true；否则，返回 false 。
+
+ 
+
+示例 1：
+
+输入：n = 4
+输出：false 
+解释：如果堆中有 4 块石头，那么你永远不会赢得比赛；
+     因为无论你拿走 1 块、2 块 还是 3 块石头，最后一块石头总是会被你的朋友拿走。
+示例 2：
+
+输入：n = 1
+输出：true
+示例 3：
+
+输入：n = 2
+输出：true
+ */
+
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        
+    }
+};
