bf4fe0022b4e31c1b0186d1a1e23e07c0536e75d
AClearSurface <282971753@qq.com>
2021-03-12 22:55:44 +0800

    36 sudo

--- Test.cpp
+++ Test.cpp
@@ -5,22 +5,19 @@
  * @E-mail              : aclearzhang@qq.com
  * @Homepage            : www.aclear.top
  * @LastEditors         : AClearZhang
- * @LastEditTime        : 2021-03-12 22:18:52
+ * @LastEditTime        : 2021-03-12 22:35:13
  * @Version             : 1.0
  * @Description         : c++语法测试
  */
 
-
-#include <iostream>
-#include <windows.h>
 #include <algorithm>
-#include <vector>
-#include <string>
 #include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
 
 using namespace std;
-
-
 
 //C++返回多个值
 //有四种方法
@@ -30,65 +27,75 @@ using namespace std;
 //4.直接声明普通数组，传入数组的首地址，形参使用指针改变数值
 
 //1.在函数内部用静态数组，最后返回数组地址
-int * get_more_param_0(){
+int *get_more_param_0()
+{
     int len = 4;
     static int a[4];
-    cout<<"-----------------------------------"<<endl;
-    for(int i=0;i<len;i++){
-        a[i] = i*10;
-        cout<<"地址："<<(a+i)<<"  数值"<<a[i]<<endl;
+    cout << "-----------------------------------" << endl;
+    for (int i = 0; i < len; i++)
+    {
+        a[i] = i * 10;
+        cout << "地址：" << (a + i) << "  数值" << a[i] << endl;
     }
-    cout<<"-----------------------------------"<<endl;
+    cout << "-----------------------------------" << endl;
     return a;
 }
 
 //2.在函数内部用指针的形式声明一个数组，最后返回一个指针首地址即可
-int * get_more_param_1(){
+int *get_more_param_1()
+{
     int len = 4;
-    int * a = new int[len];
-    cout<<"-----------------------------------"<<endl;
-    for(int i=0;i<len;i++){
-        a[i] = i*10;
-        cout<<"地址："<<(a+i)<<"  数值："<<a[i]<<endl;
+    int *a = new int[len];
+    cout << "-----------------------------------" << endl;
+    for (int i = 0; i < len; i++)
+    {
+        a[i] = i * 10;
+        cout << "地址：" << (a + i) << "  数值：" << a[i] << endl;
     }
-    cout<<"-----------------------------------"<<endl;
+    cout << "-----------------------------------" << endl;
     return a;
 }
 
 //3.在主函数中声明指针数组，然后作为参数进行计算即可，最终会改变数值
-void get_more_param_2(int * b, int start,int end){
+void get_more_param_2(int *b, int start, int end)
+{
     int len = 4;
-    cout<<"-----------------------------------"<<endl;
-    for(int i=0;i<len;i++){
-        b[i] = i*10;
-        cout<<"地址："<<(b+i)<<"  数值："<<b[i]<<endl;
+    cout << "-----------------------------------" << endl;
+    for (int i = 0; i < len; i++)
+    {
+        b[i] = i * 10;
+        cout << "地址：" << (b + i) << "  数值：" << b[i] << endl;
     }
-    cout<<"-----------------------------------"<<endl;
+    cout << "-----------------------------------" << endl;
 }
 
 //4：直接声明普通数组，传入地址，形参使用指针改变数值
-void get_more_param_3(int * b, int start,int end){
+void get_more_param_3(int *b, int start, int end)
+{
     int len = 4;
-    cout<<"-----------------------------------"<<endl;
-    for(int i=0;i<len;i++){
-        b[i] = i*10;
-        cout<<"地址："<<(b+i)<<"  数值："<<b[i]<<endl;
+    cout << "-----------------------------------" << endl;
+    for (int i = 0; i < len; i++)
+    {
+        b[i] = i * 10;
+        cout << "地址：" << (b + i) << "  数值：" << b[i] << endl;
     }
-    cout<<"-----------------------------------"<<endl;
+    cout << "-----------------------------------" << endl;
 }
 
-bool comp(int a, int b){
-    return a>b; 
+bool comp(int a, int b)
+{
+    return a > b;
 }
-int findKthLargest1(vector<int>& nums, int k) {
-        //从大到小排序 —— 找到 k-1 个未知的数。
-        int n = nums.size();
-        sort(nums.begin(), nums.begin()+n, comp);
-        return nums[k-1];
+int findKthLargest1(vector<int> &nums, int k)
+{
+    //从大到小排序 —— 找到 k-1 个未知的数。
+    int n = nums.size();
+    sort(nums.begin(), nums.begin() + n, comp);
+    return nums[k - 1];
 }
 
-
-int main(){
+int main()
+{
     // vector<int> arr = {3,1,2,4};
     // int k = 2;
     // findKthLargest1(arr, k) ;
@@ -104,61 +111,59 @@ int main(){
      */
     int a = 4;
     int b = 3;
-    float c = a / b ;
+    float c = a / b;
     cout << "Now Test caculate Values." << endl;
     cout << "c is :" << c << endl;
-    cout << "float(a) / b is:" << float(a)/b << endl; 
-    cout << "(float)a / b is:" << (float)a/b << endl; 
-    printf("a/b:%.1f\n", a/b);
-    cout << "1.0/2 = " << 1.0/2 << endl;
-    cout << "1.0/a = " << 1.0/a << endl;
+    cout << "float(a) / b is:" << float(a) / b << endl;
+    cout << "(float)a / b is:" << (float)a / b << endl;
+    // printf("a/b:%.1f\n", a / b);
+    cout << "1.0/2 = " << 1.0 / 2 << endl;
+    cout << "1.0/a = " << 1.0 / a << endl;
 
-    for(char ch='1';ch<='9';ch++){
-        cout << "Now ch is:"<<ch<<endl;
+    for (char ch = '1'; ch <= '9'; ch++)
+    {
+        cout << "Now ch is:" << ch << endl;
     }
-    
-//     cout << endl << endl << "Now Test return array Numbers" << endl;
-//     //内部声明普通数组
-//     cout<<"方法1：内部声明静态数组"<<endl;
-//     int * p = get_more_param_0();
-//     for(int i=0;i<4;i++){
-//         cout<<"地址："<<(p+i)<<"   数值："<<p[i]<<endl;
-//     }
-//     cout<<"-----------------------------------"<<endl;
-//     cout<<endl;
 
-//     //内部函数声明指针数组
-//     cout<<"方法2：内部函数声明指针数组"<<endl;
-//     int * p1 = get_more_param_1();    
-//     for(int i=0;i<4;i++){
-//         cout<<"地址："<<(p1+i)<<"   数值："<<p1[i]<<endl;
-//     }
-//     cout<<"-----------------------------------"<<endl;
-//     cout<<endl;
+    //     cout << endl << endl << "Now Test return array Numbers" << endl;
+    //     //内部声明普通数组
+    //     cout<<"方法1：内部声明静态数组"<<endl;
+    //     int * p = get_more_param_0();
+    //     for(int i=0;i<4;i++){
+    //         cout<<"地址："<<(p+i)<<"   数值："<<p[i]<<endl;
+    //     }
+    //     cout<<"-----------------------------------"<<endl;
+    //     cout<<endl;
 
-//     //直接声明指针数组，作为参数改变数组
-//     cout<<"方法3：直接声明指针数组，作为参数改变数组"<<endl;
-//     int * p2 = new int[4];
-//     get_more_param_2(p2,0,4);    
-//     for(int i=0;i<4;i++){
-//         cout<<"地址："<<(p2+i)<<"   数值："<<p2[i]<<endl;
-//     }
-//     cout<<"-----------------------------------"<<endl;
-//     cout<<endl;
+    //     //内部函数声明指针数组
+    //     cout<<"方法2：内部函数声明指针数组"<<endl;
+    //     int * p1 = get_more_param_1();
+    //     for(int i=0;i<4;i++){
+    //         cout<<"地址："<<(p1+i)<<"   数值："<<p1[i]<<endl;
+    //     }
+    //     cout<<"-----------------------------------"<<endl;
+    //     cout<<endl;
 
-//    //直接声明普通数组，传入地址，形参使用指针改变数值
-//     cout<<"方法4：直接声明普通数组，传入地址，形参使用指针改变数值"<<endl;
-//     int arr[4] = {0,0,0,0};
-//     get_more_param_3(arr,0,4);    
-//     for(int i=0;i<4;i++){
-//         cout<<"地址："<<(arr+i)<<"   数值："<<arr[i]<<endl;
-//     }
-//     cout<<"-----------------------------------"<<endl;
-//     cout<<endl;
+    //     //直接声明指针数组，作为参数改变数组
+    //     cout<<"方法3：直接声明指针数组，作为参数改变数组"<<endl;
+    //     int * p2 = new int[4];
+    //     get_more_param_2(p2,0,4);
+    //     for(int i=0;i<4;i++){
+    //         cout<<"地址："<<(p2+i)<<"   数值："<<p2[i]<<endl;
+    //     }
+    //     cout<<"-----------------------------------"<<endl;
+    //     cout<<endl;
 
-
+    //    //直接声明普通数组，传入地址，形参使用指针改变数值
+    //     cout<<"方法4：直接声明普通数组，传入地址，形参使用指针改变数值"<<endl;
+    //     int arr[4] = {0,0,0,0};
+    //     get_more_param_3(arr,0,4);
+    //     for(int i=0;i<4;i++){
+    //         cout<<"地址："<<(arr+i)<<"   数值："<<arr[i]<<endl;
+    //     }
+    //     cout<<"-----------------------------------"<<endl;
+    //     cout<<endl;
 
     system("pause");
     return 0;
 }
-
