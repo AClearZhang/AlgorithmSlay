4bacbcf9c6b5bb1eed350563b8d6f04d4b4772dc
aclearzhang <282971753@qq.com>
2021-03-15 00:30:01 +0800

    560 sub qianzhui he lastnightv2

--- C++/560.SubArrEqualK.cpp
+++ C++/560.SubArrEqualK.cpp
@@ -5,7 +5,7 @@
  * @E-mail              : aclearzhang@qq.com
  * @Homepage            : www.aclear.top
  * @LastEditors         : AClearZhang
- * @LastEditTime        : 2021-03-14 23:59:25
+ * @LastEditTime        : 2021-03-15 00:25:43
  * @Version             : 1.0
  * @Description         : 子数组的和为k
  * 560. 和为K的子数组
@@ -25,6 +25,7 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -44,7 +45,7 @@ public:
         vector<int> sum(n+1, 0);
         
         for (int i = 0; i < n; i++)
-            sum[i + 1] = sum[i] + nums[i];
+            sum[i+1] = sum[i]+nums[i];
 
         int ans = 0;
         // 穷举所有子数组
@@ -54,6 +55,34 @@ public:
                 if (sum[i] - sum[j] == k)
                     ans++;
 
+        return ans;
+    }
+    /**
+     * @Description: 方法二： 计算等式 sum[i] - k == sum[j]. //
+     * @param {vector<int>} &nums
+     * @param {int} k
+     * @return {*}
+     * @notes: 关键 使用hashmap 每次的后面j加和过程中，直接count 有几个[前缀和-k] 出现的次数。
+     */
+    int subarraySum(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        // 记录前缀和， 以及前缀和出现的次数。
+        unordered_map<int, int> preSum;
+        preSum[0] = 1;
+
+        int ans = 0, sum0_i = 0;
+        for(int i = 0; i<n ;i++){
+            sum0_i += nums[i];
+
+            int sum0_j = sum0_i - k;
+            if(preSum.count(sum0_j)){
+                ans+=preSum[sum0_j];
+                preSum[sum0_j] ++;
+            }else{ // 不存在 则加入
+                preSum.insert(sum0_i, 1);
+            }
+        }
         return ans;
     }
 };
