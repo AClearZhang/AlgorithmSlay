f47cb27f845b9c2557e1792c6ed39b3b767fffbc
aclearzhang <282971753@qq.com>
2021-03-18 23:00:08 +0800

    palindrome init

---
+++ C++/5.LongestPalindromeSubStr.cpp
@@ -0,0 +1,56 @@
+/*
+ * @FilePath            : \Algorithm&Interview\AlgorithmSlay\C++\5.LongestPalindromeSubStr.cpp
+ * @Author              : AClearZhang
+ * @Date                : 2021-03-18 22:48:50
+ * @E-mail              : aclearzhang@qq.com
+ * @Homepage            : www.aclear.top
+ * @LastEditors         : AClearZhang
+ * @LastEditTime        : 2021-03-18 22:49:16
+ * @Version             : 1.0
+ * @Description         : 最长回文子串
+ * 5. 最长回文子串
+给你一个字符串 s，找到 s 中最长的回文子串。
+
+ 
+
+示例 1：
+
+输入：s = "babad"
+输出："bab"
+解释："aba" 同样是符合题意的答案。
+示例 2：
+
+输入：s = "cbbd"
+输出："bb"
+示例 3：
+
+输入：s = "a"
+输出："a"
+示例 4：
+
+输入：s = "ac"
+输出："a"
+ 
+
+提示：
+
+1 <= s.length <= 1000
+s 仅由数字和英文字母（大写和/或小写）组成
+ */
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+    }
+};
