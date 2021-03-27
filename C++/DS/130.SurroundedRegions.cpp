2751c2d70e72b7952b3b2ea80593e5b2752c2f39
aclearzhang <282971753@qq.com>
2021-03-27 10:20:55 +0800

    new 130Solution Using UnionFind.h

--- C++/DS/130.SurroundedRegions.cpp
+++ C++/DS/130.SurroundedRegions.cpp
@@ -5,7 +5,7 @@
  * @E-mail              : aclearzhang@qq.com
  * @Homepage            : www.aclear.top
  * @LastEditors         : AClearZhang
- * @LastEditTime        : 2021-03-26 22:26:18
+ * @LastEditTime        : 2021-03-27 10:19:30
  * @Version             : 1.0
  * @Description         : 被围困的区域  —— 进一步牛刀小用--学习使用union find抽象问题、解决问题的能力。
  * 130. 被围绕的区域
@@ -42,8 +42,69 @@ board[i][j] 为 'X' 或 'O'
 #include <stack>
 #include <unordered_map>
 #include <vector>
+#include "UnionFind.h"
 
 using namespace std;
+
+/**
+ * @Description: 大材小用 —— 并查集分类 并动态连接 边界处的O为一类；虚拟出一个 dummy节点 进行处理分类。
+ * @param {*}
+ * @return {*}
+ * @notes: 我们的思路是把所有边界上的 OO 看做一个连通区域。遇到 OO 就执行并查集合并操作，这样所有的 OO 就会被分成两类
+
+和边界上的 OO 在一个连通区域内的。这些 OO 我们保留。
+不和边界上的 OO 在一个连通区域内的。这些 OO 就是被包围的，替换。
+由于并查集我们一般用一维数组来记录，方便查找 parants，所以我们将二维坐标用 node 函数转化为一维坐标。
+
+ */
+class Solution {
+public:
+    /**
+     * @Description: 首先将 边界处的O连通到dummy上；然后 附近的O与四周的O连通； 最后边界上一类的O不变，其它的O 变为X.
+     * @param {*}
+     * @return {*}
+     * @notes: 
+     */
+    void solve(vector<vector<char>>& board) {
+        vector<int> direction = {-1, 0, 1, 0, -1};
+        int m = board.size(), n = board[0].size();
+        UnionFind *unionSearch = new UnionFind(m*n+1);
+        int dummy = m*n;
+        for(int i = 0; i<m ;i++){
+            for(int j = 0; j<n ;j++){
+                if(board[i][j] == 'O'){
+                    if( i == 0 || i==(m-1)||j==0||j==(n-1)){
+                        // 边界处
+                        unionSearch->unionRoot(dummy, node(i,j,n));
+                    }else{
+                        // 非边界处，与四周的O相连接
+                        for(int k = 0;k<4;k++){
+                            int iNew=i+direction[k], jNew=j+direction[k+1];
+                            if(board[iNew][jNew] == 'O'){
+                                // 当前新的是 O，则连接起来
+                                unionSearch->unionRoot(node(i,j,n), node(iNew, jNew,n));
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        // 开始找非dummy一类的 O，替换为X
+        for(int i = 0; i<m ;i++){
+            for(int j = 0; j<n ;j++){
+                if(!unionSearch->connected(dummy, node(i,j,n))&& board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+    // helper 将i，j二维数组变换成一维数组
+    int node(int i, int j, int n){
+        return i*n + j;    
+    }
+};
 
 /**
  * @Description: 
---
+++ C++/DS/UnionFind.h
@@ -0,0 +1,116 @@
+/*
+ * @FilePath            : \Algorithm&Interview\AlgorithmSlay\C++\DS\UnionFind.h
+ * @Author              : AClearZhang
+ * @Date                : 2021-03-27 09:38:44
+ * @E-mail              : aclearzhang@qq.com
+ * @Homepage            : www.aclear.top
+ * @LastEditors         : AClearZhang
+ * @LastEditTime        : 2021-03-27 09:54:44
+ * @Version             : 1.0
+ * @Description         : 并查集封装的头文件
+ */
+
+#ifndef UNIONFIND_H
+#define UNIONFIND_H
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+/**
+ * @Description: [用法]分类 、 建立动态联通关系。 
+ * @param {*}
+ * @return {*}
+ * @notes: 
+ */
+/**
+ * @Description: 【关键】 注意每次 重量都要去改变。
+ * @param {*}
+ * @return {*}
+ * @notes: 
+ */
+class UnionFind
+{
+public:
+    UnionFind(int n)
+    {
+        this->cnt = n;
+
+        // 动态数组内存分配
+        this->parent = new int[n];
+        this->size = new int[n];
+        // this->parent = (int*)malloc(n*sizeof(int));
+        // this->size = (int*)malloc(n*sizeof(int));
+        // this->parent.resize(n); 也可以 积累！！！
+        // this->size.resize(n);
+        
+
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    // ~UnionFind()
+    // {
+    //     this->parent.clear();
+    //     this->size.clear();
+    // }
+    /* 判断 p 和 q 是否连通 */
+    bool connected(int p, int q)
+    {
+        int rootP = find(p);
+        int rootQ = find(q);
+        // cout << rootP << "rootq:" << rootQ << endl;
+        return rootP == rootQ;
+    }
+    /* 返回图中有多少个连通分量 */
+    int count()
+    {
+        return this->cnt;
+    }
+    // 发现 父节点， 返回父节点
+    int find(int x)
+    {
+        // 经过三次修正 降低路径搜索难度
+        while (parent[x] != x)
+        {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+    // 将p和q连通
+    void unionRoot(int p, int q)
+    {
+        // 比较size，将轻的放在重的上面
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ)
+            return; // cuo
+
+        if (size[rootP] > size[rootQ])
+        {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ]; // cuo
+        }
+        else
+        {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP]; //cuo
+        }
+        this->cnt--; // cuo
+    }
+
+private:
+    int cnt;       // 连通分量
+    int *parent; // 节点父节点索引 —— 方便并查集合并与使用
+    int *size;   // 每个节点的重量是多少，方便 减少查找次数
+};
+
+
+
+#endif
