ee1cf52296abb6c76794970d02d8822719c3be4a
aclearzhang <282971753@qq.com>
2021-12-07 20:13:07 +0800

    huawei test

---
+++ C++/keaiduo/HuaWeiTest.cpp
@@ -0,0 +1,50 @@
+/*
+ * @FilePath            : \AlgorithmInterview\AlgorithmSlay\C++\keaiduo\HuaWeiTest.cpp
+ * @Author              : AClearZhang
+ * @Date                : 2021-12-07 09:49:48
+ * @E-mail              : aclearzhang@qq.com
+ * @Homepage            : www.aclear.top
+ * @LastEditors         : AClearZhang
+ * @LastEditTime        : 2021-12-07 10:07:37
+ * @Version             : 1.0
+ * @Description         : 
+ */
+
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+
+
+#define For(x,y,z) for(ll x = y; x < z;++x)
+typedef long long ll;
+
+class ca{
+public:
+    int f3(){
+        this->f4;
+    }
+    static int f4();
+};
+
+int main(int argc, char const *argv[])
+{
+    char arr[] = {'a', 'b','c','d','e','f'};
+    char *p = "abcdef";
+    cout << sizeof(arr) << endl;
+    cout << sizeof(p) << endl;
+    
+    system("pause");
+    return 0;
+}
